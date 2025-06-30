9a0d56471939af75cf253c4a5b882b03519ab56c
pyerinaRC28 <devbrisanatural@gmail.com>
2025-06-29 21:59:34 -0500

    detalle evento

---
+++ js/data.js
@@ -0,0 +1,30 @@
+// data.js
+const eventos = [
+    {
+        id: 1,
+        titulo: "Noche Mali: Los chapis",
+        fecha: "10/06/2025",
+        lugar: "Explanada parque infantil Piura",
+        imagen: "../img/event-1.png",
+        estado: "finalizado",
+        descripcion: "Los Chapis, colectivo artístico multidisciplinario, toma el escenario con una propuesta provocadora: visuales digitales inspirados en símbolos afrodescendientes, beats electrónicos fusionados con zapateo criollo, y una intervención en vivo que dialoga con las obras permanentes del museo. La noche se entrelaza con testimonios performáticos sobre identidad, memoria y territorio.",
+        quiz: "Juega y gana grandes descuentos para este evento",
+        reseñas: [
+            { nombre: "Juan", fecha: "20 feb. 2025", estrellas: 5, comentario: "Fue un gran evento, muy entretenido" },
+            { nombre: "Ana", fecha: "18 feb. 2025", estrellas: 4, comentario: "Me encantó, pero el lugar estaba muy lleno" }
+        ]
+    },
+    {
+        id: 2,
+        titulo: "Encantos Andinos",
+        fecha: "30/06/2025",
+        lugar: "Universidad Privada de Piura",
+        imagen: "../img/event-2.png",
+        estado: "activo",
+        descripcion: "Evento folclórico con música andina...",
+        quiz: "Participa del sorteo con CultureQuiz",
+        reseñas: [
+            { nombre: "Luis", fecha: "30 jun. 2025", estrellas: 4, comentario: "He asistido a un evento similar, una gran experiencia" },
+        ]
+    }
+];
